3a97c24a0ab6dd6a6d48506b5557ad58667a1d45
yachehobbang <yachehobbang@naver.com>
2025-03-28 17:43:41 +0900

    프로젝트 설정 변경

--- GridCellTemperature/OnStartUp.cs
+++ GridCellTemperature/OnStartUp.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Verse;
 
-namespace GridCellTemperature
+namespace GridCellTemperature;
+
+[StaticConstructorOnStartup]
+public static class OnStartUp
 {
-	[StaticConstructorOnStartup]
-	public static class OnStartUp
+	static OnStartUp()
 	{
-		static OnStartUp()
+		var temperatureColorMapFieldInfo = AccessTools.Field(typeof(MapTemperature), "TemperatureColorMap");
+		temperatureColorMapFieldInfo.SetValue(null, new List<(float, Color)>
 		{
-			var temperatureColorMapFieldInfo = AccessTools.Field(typeof(MapTemperature), "TemperatureColorMap");
-			temperatureColorMapFieldInfo.SetValue(null, new List<(float, Color)>
-			{
-				(-100f, ColorLibrary.Black),
-				(-25f, ColorLibrary.DarkBlue),
-				(0f, ColorLibrary.Blue),
-				(25f, ColorLibrary.Green),
-				(50f, ColorLibrary.Yellow),
-				(150f, ColorLibrary.Red),
-				(300f, ColorLibrary.Magenta),
-				(600f, new Color(1f, 1f, 1f)),
-				// MapTemperature 에 TemperatureColorMap 구현이 잘못되어서 마지막에 한 번 더 넣어줌
-				(600f, new Color(1f, 1f, 1f)),
-			});
-		}
+			(-100f, ColorLibrary.Black),
+			(-25f, ColorLibrary.DarkBlue),
+			(0f, ColorLibrary.Blue),
+			(25f, ColorLibrary.Green),
+			(50f, ColorLibrary.Yellow),
+			(150f, ColorLibrary.Red),
+			(300f, ColorLibrary.Magenta),
+			(600f, new Color(1f, 1f, 1f)),
+			// MapTemperature 에 TemperatureColorMap 구현이 잘못되어서 마지막에 한 번 더 넣어줌
+			(600f, new Color(1f, 1f, 1f)),
+		});
 	}
 }
